ae2ed58336c018abfd251e6f3d1cefab987c0289
ahmadzaki8828 <ahmadzaki8828@gmail.com>
2024-08-14 00:41:29 -0400

    chatgpt route completion

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -7,4 +7,32 @@ const systemPromp = `You are a flashcard creator,Your task is to generate concis
 3) Balance content complexity to match the user's proficiency level.
 4) Suggest visual aids or mnemonics when helpful.
 5) Allow customization based on user preferences.
-6) Encourage regular review and provide feedback on common mistakes.`;
+6) Encourage regular review and provide feedback on common mistakes.
+
+Return in the following JSON format
+{
+    "flashcards":[
+        {
+            "front": str,
+            "back": str
+        }
+    ]
+}`;
+
+export async function POST(req) {
+  const openai = OpenAi();
+  const data = await req.text();
+
+  const completion = await openai.chat.completion.create({
+    messages: [
+      { role: "system", content: systemPrompt },
+      { role: "user", content: data },
+    ],
+    model: "gpt-4o",
+    response_formate_type: "json_object",
+  });
+
+  const flashcards = JSON.parse(completion.choices[0].message.content);
+
+  return NextResponse.json(flashcards.flashcard);
+}
