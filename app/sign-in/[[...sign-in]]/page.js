f9b42fa6a66ac0f36af7bf4e276cdfafb9497a11
ahmadzaki8828 <ahmadzaki8828@gmail.com>
2024-08-14 23:50:16 -0400

    sign-in page

---
+++ app/sign-in/[[...sign-in]]/page.js
@@ -0,0 +1,44 @@
+import { SignIn } from "@clerk/nextjs";
+import {
+  AppBar,
+  Container,
+  Typography,
+  Toolbar,
+  Box,
+  Button,
+} from "@mui/material";
+import Link from "next/link";
+
+export default function SignUpPage() {
+  return (
+    <Container maxWidth="sm">
+      <AppBar position="static" sx={{ backgroundColor: "#3f51b5" }}>
+        <Toolbar>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Flashcard SaaS
+          </Typography>
+          <Button color="inherit">
+            <Link href="/sign-in" passHref>
+              Login
+            </Link>
+          </Button>
+          <Button color="inherit">
+            <Link href="/sign-up" passHref>
+              Sign Up
+            </Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Typography variant="h4">Sign In</Typography>
+        <SignIn />
+      </Box>
+    </Container>
+  );
+}
